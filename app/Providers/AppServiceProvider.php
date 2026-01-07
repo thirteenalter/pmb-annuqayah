a688d66547ad919d379224a82fb1f9f94415911c
ryznxx <ryuzenfarizi@gmail.com>
2026-01-07 11:57:09 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    //
+    if (config('app.env') === 'production') {
+      URL::forceScheme('https');
     }
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+    //
+  }
 }
